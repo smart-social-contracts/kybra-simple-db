85fd7b7c3f5268367d5503d3f75a4dab52e692f7
Jose Perez <jmpmonteagudo@gmail.com>
2025-03-31 07:32:39 +0200

    refactor: Restructure database code

--- kybra_simple_db/__init__.py
+++ kybra_simple_db/__init__.py
@@ -18,7 +18,7 @@ from .properties import (
 from .storage import MemoryStorage, Storage
 from .system_time import SystemTime
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = [
     "Database",
     "Entity",
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kybra_simple_db",
-    version="0.1.1",
+    version="0.1.2",
     author="Kybra Team",
     description="A lightweight key-value database with entity relationships and audit logging",
     long_description=long_description,
