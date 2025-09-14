f38cf82a7cb46ddad60c3cb3a304460b287d02f6
GitHub Action <action@github.com>
2025-09-14 19:17:25 +0000

    Bump version: 0.3.0 → 0.3.1

--- kybra_simple_db/__init__.py
+++ kybra_simple_db/__init__.py
@@ -18,7 +18,7 @@ from .properties import (
 from .storage import MemoryStorage, Storage
 from .system_time import SystemTime
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = [
     "Database",
     "Entity",
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="kybra_simple_db",
-    version="0.3.0",
+    version="0.3.1",
     author="Smart Social Contracts",
     author_email="smartsocialcontracts@gmail.com",
     description="A lightweight key-value database with entity relationships and audit logging",
