89f28582c96cf370b674e73b9f06f7ca4d7e2d5f
GitHub Action <action@github.com>
2025-11-24 12:09:50 +0000

    Bump version: 0.4.0 → 0.5.0

--- kybra_simple_db/__init__.py
+++ kybra_simple_db/__init__.py
@@ -19,7 +19,7 @@ from .properties import (
 from .storage import MemoryStorage, Storage
 from .system_time import SystemTime
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __all__ = [
     "Database",
     "Entity",
